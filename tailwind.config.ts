f5d7933bcbe99f2ae288ec3df13e51a9cc988adb
Lamiabrc <sabullelam@gmail.com>
2025-10-31 15:26:18 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,5 +24,11 @@ const config: Config = {
   },
   plugins: [],
 }
+colors: {
+  primary: { DEFAULT: '#005B5F' },
+  secondary: { DEFAULT: '#212121' },
+  accent: { DEFAULT: '#FF6B57' },
+  border: { DEFAULT: '#E5E7EB' }, // <-- ajoute cette ligne
+},
 
 export default config
